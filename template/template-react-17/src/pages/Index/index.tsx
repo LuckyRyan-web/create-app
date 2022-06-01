fb2d2d6708fe76eaa045b2439121683fd9cb5e94
liuyuan <373005226@qq.com>
2022-06-01 17:30:28 +0800

    修改为可行性模板

--- template/template-react-17/src/pages/Index/index.tsx
+++ template/template-react-17/src/pages/Index/index.tsx
@@ -1,17 +1,35 @@
 /**
- * @author ly
- * @date 2022-03-13 18:01
- * @since 0.0.0
+ * @author liuyuan
+ * @date 2022-04-10 22:35
+ * @since 0.1.0
  */
 
 import React from 'react'
-//import classnames from 'classnames'
-//import style from './style.module.scss'
+// import classnames from 'classnames'
+import style from './style.module.scss'
+import logo from './logo.svg'
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = (props) => {
-    return <div>index</div>
+    return (
+        <div className={style.App}>
+            <header className={style.AppHeader}>
+                <img src={logo} className={style.AppLogo} alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className={style.AppLink}
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
 export default React.memo(Index)
