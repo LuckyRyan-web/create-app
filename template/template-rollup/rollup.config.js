c88dd61bf60fa170a1d1d2674fa3d6756e96e947
liuyuan <373005226@qq.com>
2023-01-25 23:44:39 +0800

    fix rollup打包会打包多余依赖的问题

--- template/template-rollup/rollup.config.js
+++ template/template-rollup/rollup.config.js
@@ -5,15 +5,21 @@ import resolve from '@rollup/plugin-node-resolve'
 // 解析 cjs 格式的包
 import commonjs from '@rollup/plugin-commonjs'
 // 压缩插件
-import { terser } from 'rollup-plugin-terser'
+// import { terser } from 'rollup-plugin-terser'
 
 import eslint from '@rollup/plugin-eslint'
+
+// 解决依赖打包进来的问题
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+const packageJson = require('./package.json')
 
 export default [
     {
         input: 'src/index.ts',
-        external: ['react'],
+        external: Object.keys(packageJson.dependencies || {}),
         plugins: [
+            peerDepsExternal(),
             resolve(),
             eslint({
                 throwOnError: true,
@@ -24,17 +30,27 @@ export default [
         ],
         output: [
             {
-                file: 'dist/index.umd.js',
-                format: 'umd',
-                name: 'Index',
-                plugins: [terser()],
-                banner: '/** Hello this is my utils */',
+                file: packageJson.main,
+                format: 'cjs',
             },
             {
-                file: 'dist/index.es.js',
+                file: packageJson.module,
                 format: 'es',
-                // plugins: [terser()],
             },
         ],
+        // output: [
+        //     {
+        //         file: 'dist/index.umd.js',
+        //         format: 'umd',
+        //         name: 'Index',
+        //         plugins: [terser()],
+        //         banner: '/** Hello this is my utils */',
+        //     },
+        //     {
+        //         file: 'dist/index.es.js',
+        //         format: 'es',
+        //         // plugins: [terser()],
+        //     },
+        // ],
     },
 ]
